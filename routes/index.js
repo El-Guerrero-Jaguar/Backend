783ec7c39b45104928cf695d05754b3f86c575be
Cesar Salas <contacto@cesarsalas.mx>
2020-12-19 06:03:19 -0600

    Add Cors

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ const bodyParser=require("body-parser");
 const RootQuery = require("../graphql/queries");
 const RootMutation =  require("../graphql/mutations");
 const schema = new GraphQLSchema({query:RootQuery , mutation: RootMutation});
+
 function api(app){
     app.use(cors());
+    
+    
     app.use("/api",
         bodyParser.json(),
         graphqlHTTP.graphqlHTTP({
