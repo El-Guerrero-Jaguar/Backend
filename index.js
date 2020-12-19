5fea14e66cd78f5e40260e0bf66f11a0d41d1c38
Cesar Salas <contacto@cesarsalas.mx>
2020-12-19 13:20:21 -0600

    back to http

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const app = express();
 const { PORT } = require("./config").config;
 const api = require("./routes");
@@ -8,9 +6,5 @@ app.get('/',(req,res) => {
     res.send('Hello world');
 });
 api(app);
-https.createServer({
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
-    passphrase: 'YOUR PASSPHRASE HERE'
-}, app)
-.listen(PORT);
+
+app.listen(PORT);
